ab7064358262520985403474c6d44fea4f0ffcde
sesha <seshasree@gmail.com>
2025-11-08 18:05:40 -0600

    studio-answers

--- collections/studio/list-and-string-conversion.py
+++ collections/studio/list-and-string-conversion.py
@@ -6,18 +6,53 @@ proto_list4 = "Comma-spaces, might, require, typing, caution"
 strings = [proto_list1, proto_list2, proto_list3, proto_list4]
 
 # a) Use the 'in' method to check to see if the words in each string are separated by commas (,), semicolons (;) or just spaces.
-
+for x in strings:
+    if ","  in x:
+        print("commas found in", x)
+    elif ";" in x:
+        print("semicolons found in", x)
+    elif " " in x:
+        print("spaces found in", x)
+    else:
+        print("none found in", x)
 
 # b) If the string uses commas to separate the words, split it into an array, reverse the entries, and then join the array into a new comma separated string.
 
-
+for x in strings:
+    if ","  in x:
+        arr = x.split(",")
+        print(arr)
+        arr.reverse()
+        new_string = ",".join(arr)
+        print(new_string)
 
 # c) If the string uses semicolons to separate the words, split it into an array, alphabetize the entries, and then join the array into a new comma separated string.
-
-
+for x in strings:
+    if ";"  in x:
+        arr = x.split(";")
+        print(arr)
+        arr.sort()
+        new_string = ";".join(arr)
+        print(new_string)
 
 # d) If the string uses spaces to separate the words, split it into an array, reverse alphabetize the entries, and then join the array into a new space separated string.
 
+for x in strings:
+    if " "  in x:
+        arr = x.split(" ")
+        print(arr)
+        arr.sort()
+        print(arr)
+        arr.reverse()
+        new_string = " ".join(arr)
+        print(new_string)
 
 
 # e) If the string uses ‘comma spaces’ to separate the list, modify your code to produce the same result as part “b”, making sure that the extra spaces are NOT part of the final string.
+for x in strings:
+    if ", "  in x:
+        arr = x.split(", ")
+        print(arr)
+        arr.reverse()
+        new_string = ",".join(arr)
+        print(new_string)
--- collections/studio/string-modification.py
+++ collections/studio/string-modification.py
@@ -2,14 +2,40 @@ my_string = "LaunchCode"
 
 
 # a) Use string methods to remove the first three characters from the string and add them to the end.
-
-
+temp = my_string[:3]
+rest = my_string[3:]
+print(rest)
+print(temp)
+mod_string = rest+temp
+print(mod_string)
 
 # Use a template literal to print the original and modified string in a descriptive phrase.
-
+print(f"Original string is {my_string}, modified string is {mod_string}")
 
 
 # b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-
-
-# c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+string_input = input("Enter the name: ")
+n = int(input("Enter number of letters to be relocated: "))
+temp = string_input[:n]
+rest = string_input[n:]
+print(rest)
+print(temp)
+mod_string = rest + temp
+print(f"Original string is {string_input}, modified string is {mod_string}")
+# c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. 
+# Also, the template literal should note the error.
+string_input = input("Enter the name: ")
+print(len(string_input))
+n = int(input("Enter number of letters to be relocated: "))
+if n> len(string_input):
+    print("Number is longer than the word. Default to 3")
+    n=3
+    error_note = "(default used - input was longer than word)"
+else:
+    error_note =""
+temp = string_input[:n]
+rest = string_input[n:]
+print(rest)
+print(temp)
+mod_string = rest + temp
+print(f"Original string is {string_input}, modified string is {mod_string}{error_note}")
