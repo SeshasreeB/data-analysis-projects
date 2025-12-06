d83d06a0e3eed6a696567166f7a2ffbf3ebb0008
sesha <seshasree@gmail.com>
2025-12-06 11:33:13 -0600

    dataman

--- data.py
+++ data.py
@@ -1,2 +1,7 @@
-print('The knights who say "Ni!"')
-print(type("3.2"))
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+butterflies=[12,34,89]
+pollinator_participants=[10,56,102]
+
+plt.scatter(pollinator_participants, butterflies)
